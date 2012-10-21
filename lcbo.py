790ca863b7b2951fe761b8960390bd5d7088bf5d
Geoff Wyatt <geoffwyatt@gmail.com>
2012-10-21 12:23:49 -0400

    updated keyiter function - recursion now working correctly. Changed max product per page to 100, reduce calls to the API

--- lcbo.py
+++ lcbo.py
@@ -1,12 +1,13 @@
 import json
 import urllib2
+import string
 import pprint
 import sys
 
 #Set up global variables - LCBO API
 def GetURL(page=1):
 	"""Grab the URL from LCBO API"""
-	lcboapi = "http://lcboapi.com/products"
+	lcboapi = "http://lcboapi.com/products?per_page=100"
 	
 	if page == 1:
 		request = urllib2.Request(lcboapi)
@@ -14,7 +15,7 @@ def GetURL(page=1):
 		result = urllib2.urlopen(request).read()
 		return result
 	else: #Append page number
-		lcboapi += '?page=%s' % page
+		lcboapi += '&page=%s' % page
 		request = urllib2.Request(lcboapi)
 		request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_8_1; en-us) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27')
 		result = urllib2.urlopen(request).read()
@@ -29,11 +30,14 @@ def JsonConvert(rawdump):
 def keyitr(dump, currentpage=1):
 	"""Iterate through data, look for item"""
 	maxpage = dump['pager']['final_page']
-	while currentpage < maxpage:
-		for i in xrange(19):
+	if currentpage != maxpage:
+		for i in xrange(0,dump['pager']['current_page_record_count']):
 			print dump['result'][i]['name']
 		currentpage += 1
-		keyitr(JsonConvert(GetURL(currentpage)),currentpage)	
+		keyitr(JsonConvert(GetURL(currentpage)),currentpage)
+	else:
+		for i in xrange(0,dump['pager']['current_page_record_count']):
+			print dump['result'][i]['name']			
 
 keyitr(JsonConvert(GetURL()))
 
